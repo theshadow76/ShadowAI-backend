456c7c899b90918e8303c23269dbe2241c74831f
Vigo Walker <vigopaul05@gmail.com>
2023-10-30 21:22:01 -0300

    we78fsef

--- env.py
+++ env.py
@@ -2,6 +2,6 @@ FIREBASE_CERTIFICATE_KEY = "shadowai-812b5-firebase-adminsdk-jtdms-129bf6829f.js
 
 class OpenAIKeys:
     OPENAI_API_KEY = "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"
-    FETCH_ALL_MESSAGES_PATH = "Backend\content\openai_fetch_all_messages.txt"
-    GENERATE_IMAGE_PATH = "Backend\content\openai_generate_image.txt"
-    SYSTEM_PATH = "Backend\content\openai_system.txt"
+    FETCH_ALL_MESSAGES_PATH = "content\openai_fetch_all_messages.txt"
+    GENERATE_IMAGE_PATH = "content\openai_generate_image.txt"
+    SYSTEM_PATH = "content\openai_system.txt"
