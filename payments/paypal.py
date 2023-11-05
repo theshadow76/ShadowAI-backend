e6162a813f4d493c9cc4576e78e75c13a43d36a0
Vigo Walker <vigopaul05@gmail.com>
2023-11-05 13:49:21 -0300

    update 089u2jdn2

--- payments/paypal.py
+++ payments/paypal.py
@@ -2,6 +2,7 @@ import requests
 from env import PaymentData
 from help.helper_functions import GenRandomString
 import base64
+import json
 
 class PayPalAPI:
     def __init__(self):
@@ -40,6 +41,27 @@ class PayPalAPI:
             "billing_cycles": billing_cycles,
             "payment_preferences": payment_preferences,
             "taxes": taxes
+        }
+        response = requests.post(url, headers=self.headers, json=data)
+        return response
+    
+    def create_subscription(self, plan_id, start_time, quantity, shipping_amount, subscriber, application_context):
+        url = 'https://api-m.sandbox.paypal.com/v1/billing/subscriptions'
+        ID = GenRandomString(10)
+        ID2 = GenRandomString(5)
+        credentials = f"{PaymentData.PAYPAL_SANDBOX_CLIENT_ID}:{PaymentData.PAYPAL_SANDBOX_SECRET_KEY_1}"
+        basic_auth = base64.b64encode(credentials.encode()).decode()
+        self.headers.update({
+            'PayPal-Request-Id': f'SUBSCRIPTION-{ID}-{ID2}',
+            'Authorization': f'Basic {basic_auth}'
+        })
+        data = {
+            "plan_id": plan_id,
+            "start_time": start_time,
+            "quantity": quantity,
+            "shipping_amount": shipping_amount,
+            "subscriber": subscriber,
+            "application_context": application_context
         }
         response = requests.post(url, headers=self.headers, json=data)
         return response
@@ -82,6 +104,21 @@ class ExecutePayPalOrder:
                     }
                 }
             },
+            {
+                "frequency": {
+                    "interval_unit": "MONTH",
+                    "interval_count": 1
+                },
+                "tenure_type": "REGULAR",
+                "sequence": 2,
+                "total_cycles": 12,  # Or another number according to your plan
+                "pricing_scheme": {
+                    "fixed_price": {
+                        "value": PaymentData.SUB_1_PRICE_USD,
+                        "currency_code": "USD"
+                    }
+                }
+            }
         ],
         payment_preferences={
             "auto_bill_outstanding": True,
@@ -98,5 +135,58 @@ class ExecutePayPalOrder:
         }
     )
         return f"The plan response is: {plan_response.json()}"
+    
+    def CreateSubscription(self):
+        plan_id = "P-5ML4271244454362WXNWU5NQ"  # Replace with actual plan_id from your CreatePlan method
+        start_time = "2018-11-01T00:00:00Z"
+        quantity = "20"
+        shipping_amount = {
+            "currency_code": "USD",
+            "value": "10.00"
+        }
+        subscriber = {
+            "name": {
+                "given_name": "John",
+                "surname": "Doe"
+            },
+            "email_address": "customer@example.com",
+            "shipping_address": {
+                "name": {
+                    "full_name": "John Doe"
+                },
+                "address": {
+                    "address_line_1": "2211 N First Street",
+                    "address_line_2": "Building 17",
+                    "admin_area_2": "San Jose",
+                    "admin_area_1": "CA",
+                    "postal_code": "95131",
+                    "country_code": "US"
+                }
+            }
+        }
+        application_context = {
+            "brand_name": "walmart",
+            "locale": "en-US",
+            "shipping_preference": "SET_PROVIDED_ADDRESS",
+            "user_action": "SUBSCRIBE_NOW",
+            "payment_method": {
+                "payer_selected": "PAYPAL",
+                "payee_preferred": "IMMEDIATE_PAYMENT_REQUIRED"
+            },
+            "return_url": "https://example.com/returnUrl",
+            "cancel_url": "https://example.com/cancelUrl"
+        }
+        subscription_response = self.paypal_api.create_subscription(
+            plan_id=plan_id,
+            start_time=start_time,
+            quantity=quantity,
+            shipping_amount=shipping_amount,
+            subscriber=subscriber,
+            application_context=application_context
+        )
+        return f"The subscription response is: {subscription_response.json()}"
+
+
+
     def execute(self):
         pass
