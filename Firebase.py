4867cd0d5b236d7b088d800d427b77e6cee8f6b3
Vigo Walker <vigopaul05@gmail.com>
2023-11-06 11:51:13 -0300

    Update 978ub2hi

--- Firebase.py
+++ Firebase.py
@@ -1,24 +1,21 @@
 import firebase_admin
-
-from firebase_admin import credentials
-from firebase_admin import firestore
+from firebase_admin import credentials, firestore
 from env import FIREBASE_CERTIFICATE_KEY
-from firebase_admin import db
 
 class FirePay:
-    def __init__(self):
-        self.ref = db.reference('users')
+    def __init__(self, key: str = None):
+        # Initialize Firebase app if not already initialized
+        if not firebase_admin._apps:
+            if key:
+                cred = credentials.Certificate(key)
+            else:
+                cred = credentials.Certificate(FIREBASE_CERTIFICATE_KEY)
+            firebase_admin.initialize_app(cred)
 
-    def add_values(self, user_id, PaymentSubID, SubType, PaymentStatus, key: str = None):
-            # Initialize Firebase app
-        if key:
-            cred = credentials.Certificate(key)  # Replace with your Firebase project's certificate path
-        else: 
-            cred = credentials.Certificate(FIREBASE_CERTIFICATE_KEY)
-        firebase_admin.initialize_app(cred, {
-            "databaseURL": "shadowai-812b5.firebaseapp.com"
-        })
-        user_ref = self.ref.child(user_id)
+        self.db = firestore.client()
+
+    def add_values(self, user_id, PaymentSubID, SubType, PaymentStatus):
+        user_ref = self.db.collection('users').document(user_id)
         try:
             user_ref.set({
                 'PaymentSubID': PaymentSubID,
@@ -27,12 +24,17 @@ class FirePay:
             })
             return {"Success": "User added to database"}
         except Exception as e:
-            return {"Error": e}
+            return {"Error": str(e)}
+
     def update_status(self, user_id, new_status):
-        user_ref = self.ref.child(user_id)
-        user_ref.update({
-            'PaymentStatus': new_status
-        })
+        user_ref = self.db.collection('users').document(user_id)
+        try:
+            user_ref.update({
+                'PaymentStatus': new_status
+            })
+            return {"Success": "User status updated"}
+        except Exception as e:
+            return {"Error": str(e)}
 
 def get_firebase_database(key: str = None):
     """
