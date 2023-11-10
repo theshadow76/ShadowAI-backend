418deb22231cd9aa3d193049ae05f6a1c0e932cd
Vigo Walker <vigopaul05@gmail.com>
2023-11-10 11:25:28 -0300

    Update 87yuj2

--- run.py
+++ run.py
@@ -89,4 +89,4 @@ def paypal():
         return {"Error": str(e)}
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001) #NOTE: Change to 5000 when deploying to production
+    app.run(debug=True, host='0.0.0.0', port=5000) #NOTE: Change to 5000 when deploying to production
